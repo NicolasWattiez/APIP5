a61f62746563b35b2a5da10ffcf6ac3e48d19e5a
Nicolas Wattiez <nicolaswattiez.contact@gmail.com>
2020-11-11 16:06:54 +0100

    create form.html

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
-from typing import Optional
-
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+from src.model import spell_number
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates/")
+
+@app.get('/')
+def read_form():
+    return 'hello world'
